d77122fae54541bda1df015f7de9ca7a47a3d87c
duanyiwen <duanyiwen@keep.com>
2023-03-23 20:13:03 +0800

    -

--- model_cut/bloom_emb_cut.py
+++ model_cut/bloom_emb_cut.py
@@ -1,14 +1,16 @@
 """
-cut by your own char set
+cut by your own char set,
+bloom use BPE tokenizer
+todo: there is still a problem, but easy to avoid.
+tokenizer can encode out a out of model vocab_size ids, you can limit it before put 'inputs' Tensor into model.
+for example:
+inputs = tokenizer.encode("한국어南京是中华民国的什么？", return_tensors="np")
+inputs[inputs >= model.config.vocab_size] = 0
 """
-import csv
 import string
 from zhon.hanzi import punctuation
-import pandas as pd
-from collections import Counter
 import torch
 from transformers import AutoTokenizer, BloomModel
-from tqdm.auto import tqdm, trange
 
 model_path = '/*/models/bloom560m'
 tokenizer = AutoTokenizer.from_pretrained(model_path)
@@ -34,8 +36,9 @@ def is_retain_token(k):
 def msize(m):
     return sum(p.numel() for p in m.parameters())
 
+
 # you can use print(model) look up the name of every layer
-print("模型embeding大小：", msize(model.word_embeddings) / msize(model))
+print("model embedding params ratio：", msize(model.word_embeddings) / msize(model))
 
 pieces_list = []
 new_idx = 0
@@ -53,8 +56,11 @@ new_emb = torch.nn.Embedding(new_size, model.word_embeddings.embedding_dim)
 # new_head = torch.nn.Linear(in_features=model.lm_head.in_features, out_features=new_size, bias=False)
 for _, new_id, old_id in pieces_list:
     new_emb.weight.data[new_id] = model.word_embeddings.weight.data[old_id]
+    # print("new_id", new_id, "old_id", old_id)
     # new_head.weight.data[new_id] = model.lm_head.weight.data[old_id]
-model.word_embeddings.weight = new_emb.weight
+# model.word_embeddings.weight = new_emb.weight
+del model.word_embeddings.weight
+model.word_embeddings = new_emb
 # model.lm_head.weight = new_head.weight
 
 model.config.__dict__['vocab_size'] = new_size
@@ -67,23 +73,93 @@ import json
 with open("/*/models/bloom560m/tokenizer.json", "r") as f:
     d = json.load(f)
 new_vocab = {}
+
+# --- only keep useful tokens in vocab and merges. can't work, maybe broken Byte-pairs
+# for t, new_id, _ in pieces_list:
+#     new_vocab[t] = new_id
+# d['model']['vocab'] = new_vocab
+#
+# merge = d['model']['merges']
+# new_merge = []
+# for m in new_merge:
+#     if m in new_vocab:
+#         new_merge.append(m)
+# d['model']['merges'] = new_merge
+#
+# merge = d['model']['merges']
+# new_merge = []
+# for m in new_merge:
+#     if m in new_vocab:
+#         new_merge.append(m)
+# d['model']['merges'] = new_merge
+
+
+# --- re-index all tokens, remain token - new_index, dropped token - 0, 会错误分词
+# cnt = 0
+# for t, new_id, _ in pieces_list:
+#     if t in d['model']['vocab']:
+#         new_vocab[t] = new_id
+#         cnt += 1
+# print('re-index:', cnt)
+#
+# for t, old in d['model']['vocab'].items():
+#     if t not in new_vocab:
+#         new_vocab[t] = 0
+#
+# print('final vocab:', len(new_vocab))
+#
+# d['model']['vocab'] = new_vocab
+
+# --- re-index all tokens, remain token - new_index, dropped token -> cnt+=1,
+# worked, but tokenizer.vocab still contains all tokens
+cnt = 0
 for t, new_id, _ in pieces_list:
-    new_vocab[t] = new_id
+    if t in d['model']['vocab']:
+        new_vocab[t] = new_id
+        cnt += 1
+print('re-index:', cnt)
+
+for t, old in d['model']['vocab'].items():
+    if t not in new_vocab:
+        new_vocab[t] = cnt
+        cnt += 1
+
+print('final vocab:', len(new_vocab))
+
 d['model']['vocab'] = new_vocab
 
-with open("*/tokenizer.json", "w") as f:
-    json.dump(d, f, ensure_ascii=False)
+
+with open("/*/models/bloom-560m-en_zh/tokenizer.json", "w", encoding='utf-8') as f:
+    json.dump(d, f, ensure_ascii=False, indent=1)
 
 # from transformers import BloomTokenizerFast
 # tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom")
 # tokenizer.get_vocab()['<unk>']
 # tokenizer.save_vocabulary()
 # tokenizer.save_vocabulary()
-#
-# new_tokenizer = T5Tokenizer('resources/your_sp.model', extra_ids=0)
-# new_tokenizer.save_pretrained('resources/your-t5-base')
-# model.save_pretrained('resources/your-t5-base')
 
-#tf_model = TFMT5ForConditionalGeneration.from_pretrained('resources/keept5-base', from_pt=True)
 
-#tf_model..save_pretrained('keep_saved_model', saved_model=True)
+from transformers import BloomForCausalLM, AutoTokenizer
+
+# --- new model
+path = '/*/models/bloom-560m-en_zh'
+tk = AutoTokenizer.from_pretrained(path)
+light = BloomForCausalLM.from_pretrained(path)
+
+inputs = tk.encode("南京是中华民国的什么？", return_tensors="pt")
+print(inputs)
+print(tk.convert_ids_to_tokens(inputs[0]))
+outputs = light.generate(inputs)
+print(outputs)
+print(tk.decode(outputs[0]))
+
+# old model
+model_path = '/*/models/bloom560m'
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = BloomForCausalLM.from_pretrained(model_path)
+inputs = tokenizer.encode("南京是中华民国的什么？", return_tensors="pt")
+print(inputs)
+print(tokenizer.convert_ids_to_tokens(inputs[0]))
+outputs = model.generate(inputs)
+print(outputs)
+print(tokenizer.decode(outputs[0]))
