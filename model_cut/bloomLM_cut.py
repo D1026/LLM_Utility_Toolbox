7ecbd5b19f34464755929749c511c1881edcbaf8
duanyiwen <duanyiwen@keep.com>
2023-03-28 15:51:56 +0800

    -

--- model_cut/bloomLM_cut.py
+++ model_cut/bloomLM_cut.py
@@ -1,20 +1,23 @@
 """
+if you have a BloomForCausalLM model that have been finetune, you can cut it by this code.
+you should notice, the lm_head and embedding layer are two independent layer.
+it means, parameters are not shared on input embedding and output embedding, this is different from T5
+
 cut by your own char set,
 bloom use BPE tokenizer
-todo: there is still a problem, but easy to avoid.
-tokenizer can encode out a out of model vocab_size ids, you can limit it before put 'inputs' Tensor into model.
-for example:
-inputs = tokenizer.encode("한국어南京是中华民国的什么？", return_tensors="np")
-inputs[inputs >= model.config.vocab_size] = 0
 """
+import csv
 import string
 from zhon.hanzi import punctuation
+import pandas as pd
+from collections import Counter
 import torch
-from transformers import AutoTokenizer, BloomModel
+from transformers import BloomForCausalLM, AutoTokenizer
+from tqdm.auto import tqdm, trange
 
-model_path = '/*/models/bloom560m'
+model_path = '/*/models/checkpoint-93600'
 tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = BloomModel.from_pretrained(model_path)
+model = BloomForCausalLM.from_pretrained(model_path)
 
 
 # rewrite this function to filter out the tokens that You won't use
@@ -37,9 +40,11 @@ def is_retain_token(k):
 def msize(m):
     return sum(p.numel() for p in m.parameters())
 
-
 # you can use print(model) look up the name of every layer
-print("model embedding params ratio：", msize(model.word_embeddings) / msize(model))
+transformer = model.transformer
+lm_head = model.lm_head
+print("模型embeding大小：", msize(transformer.word_embeddings) / msize(model))
+
 
 pieces_list = []
 new_idx = 0
@@ -53,29 +58,31 @@ for i in range(tokenizer.vocab_size):
 new_size = len(pieces_list)
 print("old_size:", tokenizer.vocab_size)
 print("new_size:", new_size)
-new_emb = torch.nn.Embedding(new_size, model.word_embeddings.embedding_dim)
-# new_head = torch.nn.Linear(in_features=model.lm_head.in_features, out_features=new_size, bias=False)
+new_emb = torch.nn.Embedding(new_size, transformer.word_embeddings.embedding_dim)
+new_head = torch.nn.Linear(in_features=lm_head.in_features, out_features=new_size, bias=False)
+
 for _, new_id, old_id in pieces_list:
-    new_emb.weight.data[new_id] = model.word_embeddings.weight.data[old_id]
+    new_emb.weight.data[new_id] = transformer.word_embeddings.weight.data[old_id]
     # print("new_id", new_id, "old_id", old_id)
-    # new_head.weight.data[new_id] = model.lm_head.weight.data[old_id]
+    new_head.weight.data[new_id] = lm_head.weight.data[old_id]
 # model.word_embeddings.weight = new_emb.weight
-del model.word_embeddings.weight
-model.word_embeddings = new_emb
-# model.lm_head.weight = new_head.weight
+del transformer.word_embeddings.weight
+del lm_head.weight
+transformer.word_embeddings = new_emb
+lm_head.weight = new_head.weight
 
 model.config.__dict__['vocab_size'] = new_size
-model.config.__dict__['_name_or_path'] = 'bigscience/bloom-560m-en_zh'
+model.config.__dict__['_name_or_path'] = 'bigscience/bloomLM-93600-en_zh'
 
-model.save_pretrained('./bloom-560m-en_zh')
+model.save_pretrained('./bloomLM-93600-en_zh')
 
 
 import json
-with open("/*/models/bloom560m/tokenizer.json", "r") as f:
+with open("/*/models/checkpoint-93600/tokenizer.json", "r") as f:
     d = json.load(f)
 new_vocab = {}
 
-# --- only keep useful tokens in vocab and merges. can't work, maybe broken Byte-pairs
+# --- can't work, maybe broken Byte-pairs
 # for t, new_id, _ in pieces_list:
 #     new_vocab[t] = new_id
 # d['model']['vocab'] = new_vocab
@@ -130,7 +137,7 @@ print('final vocab:', len(new_vocab))
 d['model']['vocab'] = new_vocab
 
 
-with open("/*/models/bloom-560m-en_zh/tokenizer.json", "w", encoding='utf-8') as f:
+with open("/*/models/bloomLM-93600-en_zh/tokenizer.json", "w", encoding='utf-8') as f:
     json.dump(d, f, ensure_ascii=False, indent=1)
 
 # from transformers import BloomTokenizerFast
@@ -139,11 +146,21 @@ with open("/*/models/bloom-560m-en_zh/tokenizer.json", "w", encoding='utf-8') as
 # tokenizer.save_vocabulary()
 # tokenizer.save_vocabulary()
 
-
+# old model
 from transformers import BloomForCausalLM, AutoTokenizer
+model_path = '/*/models/checkpoint-93600'
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = BloomForCausalLM.from_pretrained(model_path)
+
+inputs = tokenizer.encode("南京是中华民国的什么？", return_tensors="pt")
+print(inputs)
+print(tokenizer.convert_ids_to_tokens(inputs[0]))
+outputs = model.generate(inputs)
+print(outputs)
+print(tokenizer.decode(outputs[0]))
 
 # new model
-path = '/*/models/bloom-560m-en_zh'
+path = '/*/models/bloomLM-93600-en_zh'
 tk = AutoTokenizer.from_pretrained(path)
 light = BloomForCausalLM.from_pretrained(path)
 
@@ -153,14 +170,3 @@ print(tk.convert_ids_to_tokens(inputs[0]))
 outputs = light.generate(inputs)
 print(outputs)
 print(tk.decode(outputs[0]))
-
-# old model
-model_path = '/*/models/bloom560m'
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = BloomForCausalLM.from_pretrained(model_path)
-inputs = tokenizer.encode("南京是中华民国的什么？", return_tensors="pt")
-print(inputs)
-print(tokenizer.convert_ids_to_tokens(inputs[0]))
-outputs = model.generate(inputs)
-print(outputs)
-print(tokenizer.decode(outputs[0]))
